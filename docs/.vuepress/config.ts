3d4e927d9a0e43b7fc61ecc257535a2d6d210874
鱼香ROS <87068644+fishros@users.noreply.github.com>
2022-02-24 00:11:07 +0800

    [feat]:添加访问统计

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -2,7 +2,6 @@ import { defineUserConfig } from 'vuepress'
 import type { DefaultThemeOptions } from 'vuepress'
 import {navbarConfig} from './configs/navbar'
 import {sidebarConfig} from './configs/sidebar'
-import baiduAnalytics from 'vue-baidu-analytics'
 
 export default defineUserConfig<DefaultThemeOptions>({
   // 站点配置
@@ -13,7 +12,19 @@ export default defineUserConfig<DefaultThemeOptions>({
   // 主题和它的配置
   theme: '@vuepress/theme-default',
   head: [
-    ['link', {rel: 'icon', href: '/images/nav2_logo_m.png'}]
+    ['link', {rel: 'icon', href: '/images/nav2_logo_m.png'}],
+    ["script",
+      {},
+      `
+      var _hmt = _hmt || [];
+      (function() {
+        var hm = document.createElement("script");
+        hm.src = "https://hm.baidu.com/hm.js?87772a70946d6fa993807ff07f725bf4";
+        var s = document.getElementsByTagName("script")[0]; 
+        s.parentNode.insertBefore(hm, s);
+      })();
+    `
+    ]
   ],
   themeConfig: {
     navbar: navbarConfig,
--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -1,12 +1,19 @@
-import baiduAnalytics from 'vue-baidu-analytics'
+export default ({ router }) => {
+  /**
+   * 路由切换事件处理
+   */
+  router.beforeEach((to, from, next) => {
+    console.log("切换路由", to.fullPath, from.fullPath);
 
-export default ({ Vue, router }) => {
-  Vue.use(baiduAnalytics, {
-    router: router,
-    siteIdList: [
-      '87772a70946d6fa993807ff07f725bf4',
-    ],
-    isDebug: true
+    //触发百度的pv统计
+    if (typeof _hmt != "undefined") {
+      if (to.path) {
+        _hmt.push(["_trackPageview", to.fullPath]);
+        console.log("上报百度统计", to.fullPath);
+      }
+    }
+
+    // continue
+    next();
   });
 };
-
